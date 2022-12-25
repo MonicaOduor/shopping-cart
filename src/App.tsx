2880612f71413d240c824d9237e5957cc2a8b252
MonicaOduor <Monicaoduor2@gmail.com>
2022-12-25 15:50:06 +0300

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container>
+      hi
+    </Container>
+  );
+}
+
+export default App;
