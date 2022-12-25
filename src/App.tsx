2170b572f8bae23eeff42c731f508785e8b3ea1c
MonicaOduor <Monicaoduor2@gmail.com>
2022-12-25 15:57:43 +0300

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 
 function App() {
   return (
-    <Container>
-      hi
+    <Container fluid>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Container>
   );
 }
