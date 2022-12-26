a426e5b5a589138e153844bc1c22d83acf610661
MonicaOduor <Monicaoduor2@gmail.com>
2022-12-26 12:40:47 +0300

    If else statements for add to cart functionality and increase or decrease each quantity

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -9,8 +9,9 @@ type StoreItemProps = {
 };
 
 export default function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const quantity = 0;
   return (
-    <Card className="rounded-0" style={{ border: "1px solid #f43b00" }}>
+    <Card className="rounded-0 h-100" style={{ border: "1px solid #f43b00" }}>
       <Card.Img
         variant="top"
         src={imgUrl}
@@ -25,6 +26,28 @@ export default function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
           <span>{name}</span>
           <span> {formatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <button className="btn btn-outline rounded-0 w-100">
+              Add to cart
+            </button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <button className="btn btn-outline rounded-0">-</button>
+                <span>{quantity}</span>
+                <button className="btn btn-outline rounded-0">+</button>
+              </div>
+              <button className="btn btn-sm btn-outline rounded-0">Remove</button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
